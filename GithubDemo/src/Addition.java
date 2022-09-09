ab7b4bbdffb9f9eba4f41f9c5bab48fd894154e5
banupriya28rajkumar <banupriya.sai03@gmail.com>
2022-09-09 23:54:31 +0530

    Intially want to do this action

---
+++ GithubDemo/src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10;
+int y=20;
+int z=x+y;
+System.out.println("The Total is " + z);
+	}
+
+}
